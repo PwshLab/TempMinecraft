63dd916042222a42a33162a0b5767b48e8dacf62
moritzpfefferkorn@hotmail.com <moritzpfefferkorn@hotmail.com>
2021-03-11 20:16:55 +0100

    Update Invoke-Minecraft.ps1

--- Invoke-Minecraft.ps1
+++ Invoke-Minecraft.ps1
@@ -6,7 +6,7 @@ function Invoke-Minecraft {
 		$ServerURL = "https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar",
         [Parameter(Mandatory = $false, Position = 1)]
 		[String]
-		$ClientURL = "https://launcher.mojang.com/download/MinecraftInstaller.msi",
+		$ClientURL = "https://launcher.mojang.com/download/Minecraft.exe",
         [Parameter(Mandatory = $false, Position = 2)]
 		[String]
 		$ConfURL = "https://raw.github.com/mo-tec/Invoke-Minecraft/main/Conf.zip",
@@ -20,37 +20,32 @@ function Invoke-Minecraft {
     $ServerPath = Join-Path -Path $MCPath -ChildPath "/Server"
     $ServerFilePath = Join-Path -Path $ServerPath -ChildPath "/server.jar"
     $ClientPath = Join-Path -Path $MCPath -ChildPath "/Client"
-    $ClientInstallPath = Join-Path -Path $ClientPath -ChildPath "/MinecraftInstaller.msi"
-    $ClientFilePath = Join-Path -Path $ClientPath -ChildPath "/bin/Launcher.exe"
+    $ClientFilePath = Join-Path -Path $ClientPath -ChildPath "/bin/Minecraft.exe"
     $ConfTmpPath = Join-Path -Path $ClientPath -ChildPath "/Conf.zip"
-    $ConfFilePath = Join-Path -Path $ClientPath -ChildPath "/data/.minecraft/Conf.zip"
+    $ConfFilePath = Join-Path -Path $ClientPath -ChildPath "/data/.minecraft/"
 
-    if ($Clean) { Remove-Item $MCPath -Recurse -Force -Confirm -ErrorAction SilentlyContinue }
+    if ($Clean) { Remove-Item $MCPath -Recurse -Force -ErrorAction SilentlyContinue }
 
     New-Item -Path $tmp -Name MC -ItemType Directory
     New-Item -Path $MCPath -Name Server -ItemType Directory
     New-Item -Path $MCPath -Name Client -ItemType Directory
+    New-Item -Path $ClientPath -Name "bin" -ItemType Directory
+    New-Item -Path $ClientPath -Name "data" -ItemType Directory
 
     $ServerTask = (New-Object Net.WebClient).DownloadFileTaskAsync($ServerURL, $ServerFilePath)
-    $ClientTask = (New-Object Net.WebClient).DownloadFileTaskAsync($ClientURL, $ClientInstallPath)
+    $ClientTask = (New-Object Net.WebClient).DownloadFileTaskAsync($ClientURL, $ClientFilePath)
     $ConfTask = (New-Object Net.WebClient).DownloadFileTaskAsync($ConfURL, $ConfTmpPath)
 
     New-Item -Path $ServerPath -Name "eula.txt" -ItemType File -Value "eula=true"
-    New-Item -Path $ServerPath -Name "Server.bat" -ItemType File -Value "java -Xmx1024M -Xms1024M -jar server.jar nogui"
-    New-Item -Path $ClientPath -Name "bin" -ItemType Directory
-    New-Item -Path $ClientPath -Name "data" -ItemType Directory
-    New-Item -Path $ClientPath -Name "Client.bat" -ItemType File -Value '"%cd%\bin\Launcher.exe" --workDir "%cd%\data\.minecraft"'
+    New-Item -Path $ServerPath -Name "Server.bat" -ItemType File -Value "java.exe -Xmx1024M -Xms1024M -jar $ServerPath\server.jar nogui"
+    New-Item -Path $ClientPath -Name "Client.bat" -ItemType File -Value "$ClientPath\bin\Minecraft.exe --workDir $ClientPath\data\.minecraft"
     New-Item -Path (Join-Path -Path $ClientPath -ChildPath "/data") -Name ".minecraft" -ItemType Directory
 
     while (-not ($ServerTask.IsCompleted -and $ClientTask.IsCompleted -and $ConfTask.IsCompleted) ) { Start-Sleep -Milliseconds 500 }
 
-    Start-Process msiexec "/i $ClientFilePath /qn" -Wait
-    Copy-Item -Path "C:\Program Files (x86)\Minecraft Launcher\MinecraftLauncher.exe" -Destination $ClientFilePath
-    Remove-Item -Path "C:\Program Files (x86)\Minecraft Launcher" -Recurse -Force -Confirm -ErrorAction SilentlyContinue
-    Copy-Item -Path $ConfTmpPath -Destination $ConfFilePath
-    Expand-Archive -Path $ConfFilePath -Confirm -Force -ErrorAction SilentlyContinue
-    Remove-Item -Path $ConfFilePath -Force -Confirm -ErrorAction SilentlyContinue
+    Expand-Archive -Path $ConfTmpPath -DestinationPath $ConfFilePath -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path $ConfTmpPath -Force -ErrorAction SilentlyContinue
 
-    Start-Process (Join-Path -Path $ServerPath -ChildPath "Server.bat") -NoNewWindow
-    Start-Process (Join-Path -Path $ClientPath -ChildPath "Client.bat") -NoNewWindow
+    Start-Process (Join-Path -Path $ServerPath -ChildPath "Server.bat") -WorkingDirectory $ServerPath -WindowStyle Minimized
+    Start-Process (Join-Path -Path $ClientPath -ChildPath "Client.bat") -WorkingDirectory $ClientPath -WindowStyle Hidden
 }
